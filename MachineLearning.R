82fd422612b53faf1ea1686ecca99ff74c160d27
mathieu-lepoutre <56868642+mathieu-lepoutre@users.noreply.github.com>
2020-10-27 12:26:00 +0100

    v1

---
+++ MachineLearning.R
@@ -0,0 +1,11 @@
+library(readr)
+library(readr)
+
+
+
+DataShipPositions <- read.csv("~/GitHub/rnn/DataShipPositions.csv")
+ View(DataShipPositions)
+ 
+ ## Why is the dataframe with characters not with numerics ?
+ 
+ ##How do I make a rnn ?
